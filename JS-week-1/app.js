653fb1b6a0b805aa6976d0a4789c589533e32b19
ediiotero <e.otero81@gmail.com>
2023-02-01 21:11:08 -0500

    js week done

--- JS-week-1/app.js
+++ JS-week-1/app.js
@@ -1,1 +1,26 @@
-console.log('working')
+var firstName = "Eddie";
+var lastName = "Otero";
+var batman = true;
+var list = [ 'Tobi', 2, false]
+var obj = { key: 'value', name: 'Eddie'}
+
+// console.log('first name: ', firstName);
+// console.log(batman);
+// console.log(list);
+// console.log(obj);
+
+var number = 5;
+var number2 = "5";
+var number3 = 2;
+
+console.log('test 1: ', number == number2)
+console.log('test 2: ', number === number2)
+
+console.log(number + number3)
+console.log("all three: ", number + number2 + number3)
+console.log(firstName + " " + lastName)
+
+console.log("- :", number - number3)
+console.log("/ :", number / number3)
+console.log("* :", number * number3)
+console.log("% :", number % number3)
