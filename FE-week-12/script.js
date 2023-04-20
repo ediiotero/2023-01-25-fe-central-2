1a852ddb803b8d592e27cbc5109f8d2f5df2815e
ediiotero <e.otero81@gmail.com>
2023-04-19 21:26:29 -0400

    week 12 done

---
+++ FE-week-12/script.js
@@ -0,0 +1,67 @@
+$(document).ready(() => {
+    let bookList;
+
+    $.get('http://localhost:3000/posts', data => {
+        bookList = data
+    }).done(() => bookInfoList())
+
+    const bookInfoList = () => {
+        $('#content').empty()
+        bookList.forEach(book => {
+            $('#content').append(
+                `<div class="info-box">
+                ID: ${book.id} ${book.title} ${book.author}
+                <button id=${book.id} class="btn btn-danger btn-sm">X</button>
+                </div>`
+            )
+            $(`#${book.id}`).click(() => removeItem(book.id))
+        });
+    }
+
+    const removeItem = id => {
+        $.ajax({ 
+            url: `http://localhost:3000/posts/${id}`,
+            type: 'DELETE',
+            success: function() {
+                bookInfoList()
+            }
+        })
+    }
+
+    $('#myForm').submit(event => {
+        event.preventDefault()
+        const formData = {
+            title: $('#title').val(),
+            author: $('#author').val()
+        }
+
+        $.post(
+            'http://localhost:3000/posts',
+            formData,
+            data => alert(`data added: Title: ${data.title}, Author: ${data.author}`)
+        )
+
+        $('#myForm').trigger('reset')
+        bookInfoList()
+    })
+
+    $('#myUpdateForm').submit(event => {
+        event.preventDefault()
+        const formData = {
+            id: $('#updateId').val(),
+            title: $('#updateTitle').val(),
+            author: $('#updateAuthor').val(),
+        }
+
+        $.ajax({
+            url: `http://localhost:3000/posts/${formData.id}`,
+            type: 'PUT',
+            contentType: 'application/json',
+            data: JSON.stringify(formData)
+        }).done(() => bookInfoList())
+
+        $('#myUpdateForm').trigger('reset')
+    })
+
+
+})
